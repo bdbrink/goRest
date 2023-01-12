a926f0f14b1cfc74051ae1c63d736d06ce384f67
bdbrink <brendan.brink@outlook.com>
2023-01-11 18:05:50 -0600

    int commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # goRest
-Golang rest API server
+Golang rest server
+
+## what this is for
+
+In order to understand the backend components that go into products such as data dog, elasticsearch, opensearch, etc.
+
+## Why
+
+Most outages I've exp come from data handling, interacting with the APIs and handling specific data in order to transfrom into a business requirement.
+
---
+++ rest.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// credentials to interact with server
+type User struct {
+	Username string `json:"user"`
+	Password string `json:"password"`
+}
+
+var user User
+
+// may need to open port otherwise server will fail to server traffic
+var PORT = ":1234"
+var DATA = make(map[string]string)
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusNotFound)
+	Body := "Thanks for visiting! \n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	t := time.Now().Format(time.RFC1123)
+	Body := "The current time is: " + t + "\n"
+	fmt.Fprintf(w, "%s", Body)
+}
+
+func addHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
+	if r.Method != http.MethodPost {
+		http.Error(w, "Error:", http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "%s\n", "Method not allowed!")
+		return
+	}
+
+	d, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error:", http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(d, &user)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error:", http.StatusBadRequest)
+		return
+	}
+
+	if user.Username != "" {
+		DATA[user.Username] = user.Password
+		log.Println(DATA)
+		w.WriteHeader(http.StatusOK)
+	} else {
+		http.Error(w, "Error:", http.StatusBadRequest)
+		return
+	}
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
+	if r.Method != http.MethodGet {
+		http.Error(w, "Error:", http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "%s \n", "Method not Allowed!")
+		return
+	}
+
+	d, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "ReadAll - Error", http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(d, &user)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Unmarshal - Error", http.StatusBadRequest)
+		return
+	}
+	fmt.Println(user)
+
+	_, ok := DATA[user.Username]
+	if ok && user.Username != "" {
+		log.Println("Found!")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%s \n", d)
+	} else {
+		log.Println("Not Found!")
+		w.WriteHeader(http.StatusNotFound)
+		http.Error(w, "Map - Resource not found!", http.StatusNotFound)
+	}
+	return
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host, r.Method)
+	if r.Method != http.MethodDelete {
+		http.Error(w, "Error:", http.StatusMethodNotAllowed)
+		fmt.Fprintf(w, "%s \n", "Method not allowed!")
+		return
+	}
+
+	d, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "ReadAll - Error", http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(d, &user)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Unmarshal - Error", http.StatusBadRequest)
+		return
+	}
+	log.Println(user)
+
+	_, ok := DATA[user.Username]
+	if ok && user.Username != "" {
+		if user.Password == DATA[user.Username] {
+			delete(DATA, user.Username)
+			w.WriteHeader(http.StatusOK)
+			fmt.Fprintf(w, "%s \n", d)
+			log.Println(DATA)
+		}
+	} else {
+		log.Println("User", user.Username, "Not found!")
+		w.WriteHeader(http.StatusNotFound)
+		http.Error(w, "Delete - Resource not found!", http.StatusNotFound)
+	}
+	log.Println("After:", DATA)
+	return
+}
+
+func main() {
+	args := os.Args
+	if len(args) != 1 {
+		PORT = ":" + args[1]
+	}
+
+	mux := http.NewServeMux()
+	s := &http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		IdleTimeout:  10 * time.Second,
+		ReadTimeout:  time.Second,
+		WriteTimeout: time.Second,
+	}
+
+	mux.Handle("/time", http.HandlerFunc(timeHandler))
+	mux.Handle("/add", http.HandlerFunc(addHandler))
+	mux.Handle("/get", http.HandlerFunc(getHandler))
+	mux.Handle("/delete", http.HandlerFunc(deleteHandler))
+	mux.Handle("/", http.HandlerFunc(defaultHandler))
+
+	fmt.Println("Ready to serve at", PORT)
+	err := s.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
